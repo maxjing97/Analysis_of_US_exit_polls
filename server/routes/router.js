34e4f2f55c2fadd1f41aa0c3c3ad486c6aed85e3
Max Jing <maxjing97@gmail.com>
2023-08-18 16:56:51 -0500

    added json data and moved the data folder inside the source folder.

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/users", (req, res) => {
-    data = "random message"
+    var data = "random message"
     res.send(data)
 }) 
 
