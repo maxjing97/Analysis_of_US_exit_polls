91950c83ce568252a07017b230843519b816f329
Max Jing <maxjing97@gmail.com>
2023-08-18 11:46:42 -0500

    Added options for the 2016 presidential election page

--- src/subpages_rendering/render_2016_presidential.js
+++ src/subpages_rendering/render_2016_presidential.js
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom/client';
 //render results and data analysis of the 2016 congressional election, connecting its own specialized js file that renders 
 // text and data visualizations using the corresponding webscraped data
 
-//main class to be rendered 
-class Main extends React.Component {
+//main class to be rendered, allows users to select different levels of elections 
+class MainStarting extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
@@ -15,7 +15,20 @@ class Main extends React.Component {
     
     render() {
       return (
-        <h2>2016 congressional election results compared to predicted results based on the exit polls</h2>
+        <div id="rendered_content">
+          <h2>2016 congressional election results compared to predicted results based on the exit polls</h2>
+          <h4>select an area (National or state level)</h4>
+          <select name="elections" class="select_election">
+              <option value="None">None</option>
+              <option value="National">National</option>
+            <optgroup label="State Level Elections">
+              <option value="Michigan">Michigan</option>
+              <option value="Ohio">Ohio</option>
+              <option value="Pennsylvania">Pennsylvania</option>
+              <option value="Wisconsin">Wisconsin</option>
+            </optgroup>
+          </select>
+        </div>
       )
     };
 };
@@ -23,6 +36,6 @@ class Main extends React.Component {
 
 export default function render_2016_presidential() {
     const root = ReactDOM.createRoot(document.getElementById('content'));
-    const element = <Main/>;
+    const element = <MainStarting/>;
     root.render(element);
 }
